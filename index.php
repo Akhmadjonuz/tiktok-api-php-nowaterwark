8366b2b3b9ac29d612420ed8c49aaed776e79209
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-05-29 15:20:49 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$url = $_GET['url'];
+$id = $_GET['id'];
+$video = file_get_contents("https://tikdown.org/getAjax?url=" . urlencode($url) . "&_token=t4JdZ9etWtvCbI68c77THtVyYJEtEp4DrGg2Af8W");
+$live_video = json_decode($video, true);
+
+
+$links = [];
+$ex = explode('href="', $live_video['html']);
+$result = explode('" name="download"', $ex[1]);
+array_push($links, $result[0]);
+file_put_contents('tiktok/' . $id . '.mp4', file_get_contents($result[0]));
+$result = explode('" name="download"', $ex[2]);
+array_push($links,  $result[0]);
+file_put_contents('tiktok/' . $id . '.mp3', file_get_contents($result[0]));
+
+echo json_encode($links);
