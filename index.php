c89d8b759751dd50a9c33d3f837fc39df7c4ac05
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-06-08 12:52:05 +0500

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ file_put_contents('tiktok/' . $id . '.mp4', file_get_contents($result[0]));
 $result = explode('" name="download"', $ex[2]);
 
 array_push($links,  $result[0]);
+
 file_put_contents('tiktok/' . $id . '.mp3', file_get_contents($result[0]));
 
 echo json_encode($links);
