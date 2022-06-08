83b8d213227e38ded4cfaff952abb0000681e693
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-06-08 12:51:39 +0500

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ $ex = explode('href="', $live_video['html']);
 $result = explode('" name="download"', $ex[1]);
 
 array_push($links, $result[0]);
+
 file_put_contents('tiktok/' . $id . '.mp4', file_get_contents($result[0]));
 $result = explode('" name="download"', $ex[2]);
 array_push($links,  $result[0]);
