a6a071ebf5583f63bd2f705434e94d6a98b9e3f5
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-06-20 02:26:29 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-# Mualliflik huquqi saqlansin!
-# dadabayev.uz
-# +998902224311
-# telegram: @Akhmadjon
-
-# Copyright reserved!
-# dadabayev.uz
-# +998902224311
-# telegram: @Akhmadjon
-
-# Авторские права защищены!
-# dadabayev.uz
-# +998902224311
-# telegram: @Akhmadjon
 
 $url = $_GET["url"];
 $id = $_GET["id"];
