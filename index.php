27599f442a3aabf6c445e38e9c732e1e30463541
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-07-02 23:52:32 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 
-$url = $_GET["url"];
-$id = $_GET["id"];
-$video = file_get_contents("https://tikdown.org/getAjax?url=" . urlencode($url) . "&_token=t4JdZ9etWtvCbI68c77THtVyYJEtEp4DrGg2Af8W");
-$live_video = json_decode($video, true);
+@$url = $_GET["url"];
+@$id = $_GET["id"];
+@$video = file_get_contents("https://tikdown.org/getAjax?url=" . urlencode($url) . "&_token=t4JdZ9etWtvCbI68c77THtVyYJEtEp4DrGg2Af8W");
+@$live_video = json_decode($video, true);
 mkdir("tiktok");
 
-$links = [];
+@$links = [];
 
-$ex = explode('href="', $live_video['html']);
+@$ex = explode('href="', $live_video['html']);
 
-$result = explode('" name="download"', $ex[1]);
+@$result = explode('" name="download"', $ex[1]);
 
 array_push($links, $result[0]);
 
 file_put_contents('tiktok/' . $id . '.mp4', file_get_contents($result[0]));
 
-$result = explode('" name="download"', $ex[2]);
+@$result = explode('" name="download"', $ex[2]);
 
 array_push($links,  $result[0]);
 
